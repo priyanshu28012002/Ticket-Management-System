604be575468c389fce7658c866324c05a169c933
Priyanshu_Srivastava <priyanshusrivastav008@gmail.com>
2023-08-17 06:00:24 +0530

    day 3 solver

---
+++ models/Solver.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const SolverSchema = new mongoose.Schema({
+  name:String,
+  solverId:String,
+  email: String,
+  categorie: String,
+  password: String,
+});
+
+const Solver = mongoose.model("Solver", SolverSchema);
+
+module.exports = Solver;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const db = require("./models/db");
 const ClientModel = require("./models/Client");
 const { generateKey } = require("crypto");
 const TicketsModel = require("./models/Tickets");
+const SolverModel = require("./models/Solver");
 
 // Static folder for serving HTML, CSS, JS, and other assets
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +17,7 @@ app.set("view engine", "ejs"); // Set EJS as the view engine
 app.set("views", path.join(__dirname, "public/views"));// Set the views directory
 // Setup sessions
 app.use(session({
-    secret: "a",
+    secret: "a", 
     resave: false,
     saveUninitialized: true
 }));
@@ -146,14 +147,192 @@ app.post("/newTicketForm",async function(req,res){
         res.status(500).send("An error occurred");
     }
   }
-})  
+})
+
+app.get("/solverSignup",function(req,res){
+    res.render("solverSignup",{error : null});
+})
+ 
+app.post("/solverSignup", async function (req, res) {
+    const { name, email, categorie, password } = req.body;
+
+    try {
+        const solverId = generateSolverId(name);
+        console.log("the Solver id is " + solverId);
+
+        console.log(name, solverId, email, categorie, password);
+
+        const existingUser = await SolverModel.findOne({ email });
+
+        if (existingUser) {
+            return res.render("signup", { error: "Email is already registered" });
+        }
+
+        const newUser = await SolverModel.create({
+            name,
+            solverId,
+            email,
+            categorie,
+            password
+        });
+
+        req.session.isLoggedIn = true;
+        req.session.email = email;
+        req.session.solverId = solverId;
+        req.session.categories = categorie;
+        req.session.name = name;
+
+        return res.render("admin");
+    } catch (error) {
+        console.error("Error:", error);
+        return res.render("solverSignup", { error: "An error occurred" });
+    }
+});
+
+app.get("/solverLogin", function (req, res) {
+    res.render("solverLogin", { error: null });
+});
+
+app.post("/solverLogin", async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const user = await SolverModel.findOne({ email, password });
+
+        if (user) {
+            req.session.isLoggedIn = true;
+            req.session.email = email;
+
+            // Retrieve additional solver data from the database
+            const solverData = await SolverModel.findOne({ email });
+
+            if (solverData) {
+                const { name, solverId, categorie } = solverData;
+                console.log("the data id "+categorie)
+                req.session.solverId = solverId;
+                req.session.name = name;
+                req.session.categorie = categorie;
+                res.redirect("/solverDashboard");
+            } else {
+                res.render("solverLogin", { error: "Solver data not found" });
+            }
+        } else {
+            res.render("solverLogin", { error: "Invalid username or password" });
+        }
+    } catch (err) {
+        console.error("Error:", err);
+        res.render("solverLogin", { error: "An error occurred" });
+    }
+});
+
+app.get("/solverDashboard", async function(req, res) {
+    if (!req.session.isLoggedIn) {
+        res.redirect("/solverLogin");
+    } else {
+        try {
+            console.log("i am here")
+            const categories = req.session.categorie;
+            const tickets = await TicketsModel.find({ categories });
+            console.log(tickets)
+            res.render("solverDashboard", { tickets: tickets });
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+});
+
+app.post("/updateTicketStatus", async function (req, res) {
+    if (!req.session.isLoggedIn) {
+        return res.redirect("/solverLogin");
+    }
+
+    const ticketId = req.body.ticketId;
+    const newStatus = req.body.newStatus;
+
+    try {
+        // Update the ticket status
+        await TicketsModel.updateOne({ ticketId: ticketId }, { status: newStatus });
+
+        res.redirect("/solverDashboard");
+    } catch (err) {
+        console.error("Error:", err);
+        res.render("solverDashboard", { error: "An error occurred" });
+    }
+});
+
+app.post("/addSolution", async function (req, res) {
+    if (!req.session.isLoggedIn) {
+        return res.redirect("/solverLogin");
+    }
+
+    const ticketId = req.body.ticketId;
+    const solution = req.body.solution;
+
+    try {
+        // Update the ticket's solution
+        await TicketsModel.updateOne({ ticketId: ticketId }, { solution: solution });
+
+        res.redirect("/solverDashboard");
+    } catch (err) {
+        console.error("Error:", err);
+        res.render("solverDashboard", { error: "An error occurred" });
+    }
+});
+
+app.post("/updateTicket", async function (req, res) {
+    if (!req.session.isLoggedIn) {
+        return res.redirect("/solverLogin");
+    }
+
+    const ticketId = req.body.ticketId;
+    const newStatus = req.body.newStatus;
+    const solution = req.body.solution;
+
+    try {
+        // Update the ticket status and solution
+        await TicketsModel.updateOne(
+            { ticketId: ticketId },
+            { status: newStatus, solution: solution }
+        );
+
+        res.redirect("/solverDashboard");
+    } catch (err) {
+        console.error("Error:", err);
+        res.render("solverDashboard", { error: "An error occurred" });
+    }
+});
+
+
+app.get("/adminLogin",function(req,res){
+    res.render("adminLogin",{error :null});
+})
+
+app.post("/adminLogin", async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        if (email === "10" && password === "10") {
+            req.session.isLoggedIn = true;
+            req.session.email = email;
+            res.render("admin");
+        } else {
+            res.render("adminLogin", { error: "Invalid username or password" });
+        }
+    } catch (err) {
+        res.render("adminLogin", { error: "An error occurred" });
+    }
+});
+
+
 //Logout route to clear session and redirect to login page
 app.get("/logout", function (req, res) {
     req.session.destroy(function (err) {
       if (err) {
         console.log(err);
       }
-      res.redirect("/login");
+      res.redirect("/");
     });
   });
 
@@ -174,6 +353,11 @@ db.init()
 
 
 function generateClientId(name) {
+    const randomNumber = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+    return `@${name}${randomNumber}`;
+}
+
+function generateSolverId(name) {
     const randomNumber = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
     return `@${name}${randomNumber}`;
 }
